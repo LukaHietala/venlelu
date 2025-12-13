3127222f3382a71918eaf99694a0787af44cd24d
Luka Hietala <luka.hietala08@gmail.com>
2025-12-13 19:07:48 +0200

    dont try to resolve partitions, just refuse

--- writer.py
+++ writer.py
@@ -4,34 +4,18 @@ from pathlib import Path
 class DeviceVerifyError(Exception):
     pass
 
-# TODO: not all devices have ro
-def is_readonly(root_block):
+def is_readonly(root_block_name: str) -> bool:
     # /sys/block/<disk>/ro, 0 - not read only, 1 read only
     # https://www.kernel.org/doc/Documentation/ABI/stable/sysfs-block
-    return (Path("/sys/block") / root_block / "ro").read_text().strip() == "1"
+    return (Path("/sys/block") / root_block_name / "ro").read_text().strip() == "1"
 
 def is_block_name(name: str) -> bool:
     return (Path("/sys/class/block") / name).exists()
 
-def root_block_name(dev):
-    # /dev/sda1 -> sda, /dev/nvme0n1p2 -> nvme0n1, /dev/sda -> sda
-    name = os.path.basename(dev)
-    sys_class = Path("/sys/class/block") / name
-    if not sys_class.exists():
-        raise FileNotFoundError(f"no sysfs entry for {dev}")
+def is_partition(name: str) -> bool:
+    return (Path("/sys/class/block") / name / "partition").exists()
 
-    real = sys_class.resolve()
-    parts = real.parts
-    if "block" not in parts: # block should always be part of path
-        return name
-
-    i = parts.index("block")
-    if i + 1 < len(parts): 
-        return parts[i + 1]
-    else:
-        return name
-
-def verify_device(block_device):
+def verify_device(block_device: str) -> str:
     """
     Checks if device is valid root block device
     Returns root name of the provided device
@@ -45,9 +29,10 @@ def verify_device(block_device):
     if not is_block_name(name):
         raise DeviceVerifyError(f"not a block device: {block_device}")
 
-    root = root_block_name(block_device)
-    if root != name:
-        raise DeviceVerifyError("refusing to write to partition. give root!")
+    if is_partition(name):
+        raise DeviceVerifyError("refusing to write to partition. give root block device!")
+
+    root = name
 
     try:
         if is_readonly(root):
@@ -62,7 +47,7 @@ def verify_device(block_device):
 def check_image():
     pass
 
-def get_args(argv):
+def get_args(argv) -> tuple[str, str]:
     image_file = None  # path to iso, e.g. debian.iso (TODO: always use this, DONT make it an option)
     block_device = None   # path to disk, e.g. /dev/sda
 
@@ -91,7 +76,12 @@ def get_args(argv):
 
 def main():
     src, dst = get_args(sys.argv[1:])
-    root = verify_device(dst)
+    root = ""
+    try:
+        root = verify_device(dst)
+    except DeviceVerifyError as e:
+        print(e, file=sys.stderr)
+
     print(root)
     pass
 
